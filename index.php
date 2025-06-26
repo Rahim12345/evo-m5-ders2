f274d0efa63bc82e2ddd14abf957e38400a25c58
Rahim12345 <rahim.suleymanov94@gmail.com>
2025-06-26 15:28:25 +0400

    2-ci addim

--- index.php
+++ index.php
@@ -8,25 +8,7 @@
 </head>
 
 <body>
-    <b>qalin</b>
-    <br>
-    <i>kursiv yazi</i>
-    <br>
-    <u>underline</u>
-    <hr>
-    <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel libero vel turpis semper commodo. Sed consectetur, nisi id tincidunt dictum, justo velit sagittis velit, et consectetur ipsum nisi vitae velit. Sed vel libero vel turpis semper commodo. Sed consectetur, nisi id tincidunt dictum, justo velit sagittis velit, et consectetur ipsum nisi vitae velit.
-    </p>
-    <!-- unordered list -->
-     <?php
-
-        $list = ['Birinci element', 'İkinci element', 'Üçüncü element','Dördüncü element', 'Beşinci element'];
-     ?>
-    <ul>
-        <?php foreach ($list as $item) {?>
-            <li><?php echo $item;?></li>
-        <?php }?>
-    </ul>
+    <h1>Git dersleri</h1>
 
 </body>
 
